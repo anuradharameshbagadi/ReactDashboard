bd7fa488fc9762750f908d9b8e49ba8203b6e4fe
anuradharameshbagadi <anubagadibagadi@gmail.com>
2026-09-09 14:51:58 +0530

    updated homepage

--- src/Components/pages/home/Home.jsx
+++ src/Components/pages/home/Home.jsx
@@ -1,12 +1,39 @@
 import React from 'react'
+import { useState } from 'react';
+
 
 const Home = () => {
+
+const getAlert = () =>{
+  alert("Button Was Clicked");
+}
+
+const [number,setNumber]= useState(0);
+
+const increment = () =>{
+  setNumber(number + 1);
+}
+
+  const decrement = () =>{
+    //setNumber(number - 1);
+    if (number > 0) {
+      setNumber(number - 1);
+    }
+  }
+
+
   return (
-   <>
-   <h1>This is Home Page</h1>
+    <>
    
-   
-   </>
+    <button onClick={getAlert}>Click Me</button>
+
+    <button onClick={increment}>Increment</button>
+
+    <h1>Number : {number}</h1>
+
+    <button onClick={decrement}>Decrement</button>
+    
+    </>
   )
 }
 
